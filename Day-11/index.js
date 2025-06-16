56551a4ca966e37ad57f6e28802009ed2ec1578f
sravanis04 <ssravanitulasi@gmail.com>
2025-06-16 11:29:24 +0530

    Day-12 Assignment

---
+++ Day-11/index.js
@@ -0,0 +1,12 @@
+//console.log(userName)
+//var userName="sravani"
+//console.log(userName)
+
+var name="Sravani"
+var password="67548765"
+function printName(){
+    
+    console.log(name, password)
+}
+printName()
+console.log(name)
